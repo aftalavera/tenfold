315c9b8143958c23b1970b638ee948b1d5a83d7b
Angel Talavera <aftalavera@sdipr.net>
2015-10-06 13:41:23 -0500

    Moving the login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,21 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, SelectField
 from wtforms.validators import required, email
+from ..models import City
+from .. import db
 
 
 class LoginForm(Form):
     email = StringField('Email :', validators=[required('Please enter email.'), email()])
     password = PasswordField('Password :', validators=[required('Please enter password.')])
+    # department = SelectField('Department :', coerce=int)
     submit = SubmitField('Login')
+
+    # def __init__(self, *args, **kwargs):
+    #     super(LoginForm, self).__init__(*args, **kwargs)
+    #     query = db.session.query(City.department_nam.distinct().label("department"))
+    #     # self.department.choices = [(1, row.department) for row in query.order_by('department').all()]
+    #     self.department.choices = [(1, 'San Juan')]
 
 
 
